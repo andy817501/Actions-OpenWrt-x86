295d3e14e02bd143b7c3ab6cb8376ca9cfeeb500
andy817501 <105210605+andy817501@users.noreply.github.com>
2024-07-16 09:59:35 +0800

    sh changge

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 #git checkout b201c29fa2
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-# git clone https://github.com/pymumu/luci-app-smartdns -b lede package/lean/luci-app-smartdns
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/pymumu/luci-app-smartdns -b lede package/lean/luci-app-smartdns
